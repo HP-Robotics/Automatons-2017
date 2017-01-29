993b3eaf3c398ae38748ad389cfe32e0c4be254e
Team 2823 Programmers <teamautomatons2823@gmail.com>
2017-01-29 13:12:03 -0600

    Migrated code from Newx3-Automatons-2017
    Migrated correct code from a different project.

--- src/org/usfirst/frc/team2823/robot/Robot.java
+++ src/org/usfirst/frc/team2823/robot/Robot.java
@@ -9,21 +9,26 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import com.ctre.CANTalon;
 import com.kauailabs.navx.frc.AHRS;
 
 public class Robot extends IterativeRobot {
-    Jaguar motor1;
-	Jaguar motor2;
+    CANTalon shooter;
+	VictorSP intake;
+	VictorSP uptake;
 	
 	Compressor c;
 	DoubleSolenoid solenoid1;
 	double count = 0;
 	boolean solenoidForward = false;
+    String direction = "forward";
 	
 	Joystick stick1;
 	Joystick stick2;
@@ -48,19 +53,21 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		stick1 = new Joystick (0);
-		stick2 = new Joystick(1);
-        motor1 = new Jaguar(0);
-        SmartDashboard.putNumber("Speed", 0.0);
-        motor2 = new Jaguar(1);
-        SmartDashboard.putBoolean("M2", false);
+		//stick2 = new Joystick(1);
+        shooter = new CANTalon(0);
+        SmartDashboard.putNumber("Shooter", 0.0);
+        intake = new VictorSP(4);
+        SmartDashboard.putNumber("Intake", 0.0);
+        uptake = new VictorSP(5);
+        SmartDashboard.putNumber("Uptake", 0.0);
         
         ahrs = new AHRS(I2C.Port.kOnboard);
     	gyro = new ADXRS450_Gyro(Port.kOnboardCS0);
         
-        c = new Compressor(0);
-        c.setClosedLoopControl(true);
+        //c = new Compressor(0);
+        //c.setClosedLoopControl(true);
         
-        solenoid1 = new DoubleSolenoid(0,1);
+        //solenoid1 = new DoubleSolenoid(0,1);
         
         count = Timer.getFPGATimestamp();
         
@@ -99,37 +106,54 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-    	double lSpeed = stick1.getRawAxis(1);
+    	//double lSpeed = stick1.getRawAxis(1);
     	//double rSpeed = -stick2.getRawAxis(1);
     	//motor1.set(lSpeed);
     	//System.out.println("Speed: " + lSpeed);
     	//motor2.set(rSpeed);
     	//System.out.println("NavX: "+ ahrs.getAngle());
-    	navxDriftFromOrigin = Math.abs(ahrs.getFusedHeading()-navxOrigin);
-    	navx2DriftFromOrigin =  Math.abs(ahrs.getCompassHeading()-navx2Origin);
-    	System.out.println("Navx Fused Origin Drift: " + navxDriftFromOrigin);
-    	System.out.println("Navx Compass Origin Drift: " + navx2DriftFromOrigin);
+    	//navxDriftFromOrigin = Math.abs(ahrs.getFusedHeading()-navxOrigin);
+    	//navx2DriftFromOrigin =  Math.abs(ahrs.getCompassHeading()-navx2Origin);
+    	//System.out.println("Navx Fused Origin Drift: " + navxDriftFromOrigin);
+    	//System.out.println("Navx Compass Origin Drift: " + navx2DriftFromOrigin);
     	//System.out.println("Gyro: " + gyro.getAngle());
-    	gyroDriftFromOrigin = Math.abs(gyro.getAngle()-gyroOrigin);
-    	System.out.println("Gyro Origin Drift: " + gyroDriftFromOrigin);
+    	//gyroDriftFromOrigin = Math.abs(gyro.getAngle()-gyroOrigin);
+    	//System.out.println("Gyro Origin Drift: " + gyroDriftFromOrigin);
     	
-    	yButtonClicked = stick1.getRawButton(4);
-    	aButtonClicked = stick1.getRawButton(2);
-    	if(yButtonClicked){
-    		yButtonWasClicked = true;
+//    	yButtonClicked = stick1.getRawButton(4);
+//    	aButtonClicked = stick1.getRawButton(2);
+//    	if(yButtonClicked){
+//    		yButtonWasClicked = true;
+//    	}
+//    	if(aButtonClicked){
+//    		aButtonWasClicked = true;
+//    	}
+//    	if(yButtonWasClicked && rampSpeed < 1.0 && !yButtonClicked){
+//    		rampSpeed = (rampSpeed + 0.05);
+//    		yButtonWasClicked = false;
+//    	}else if(aButtonWasClicked && rampSpeed > -1.0 && !aButtonClicked){
+//    		rampSpeed = (rampSpeed - 0.05);
+//    		aButtonWasClicked = false;
+//    	}   	
+//    	motor1.set(rampSpeed);
+//    	System.out.println(rampSpeed);
+    	if(stick1.getRawButton(1)){
+    		intake.set(1.0);
+    		uptake.set(1.0);
+    		//intake.set(SmartDashboard.getNumber("Intake", 0.0));
+    		//intake.set(SmartDashboard.getNumber("Uptake", 0.0));
+    	}else{
+    		intake.set(0.0);
+    		uptake.set(0.0);
     	}
-    	if(aButtonClicked){
-    		aButtonWasClicked = true;
+    	if(stick1.getRawButton(2)){
+    		//shooter.set(-0.75);
+    		shooter.set(SmartDashboard.getNumber("Shooter", 0.0));
+    	}else{
+    		shooter.set(0.0);
     	}
-    	if(yButtonWasClicked && rampSpeed < 1.0 && !yButtonClicked){
-    		rampSpeed = (rampSpeed + 0.05);
-    		yButtonWasClicked = false;
-    	}else if(aButtonWasClicked && rampSpeed > -1.0 && !aButtonClicked){
-    		rampSpeed = (rampSpeed - 0.05);
-    		aButtonWasClicked = false;
-    	}   	
-    	motor1.set(rampSpeed);
-    	System.out.println(rampSpeed);
+    	
+    	
     	//System.out.println("Gyro: " + gyro.getAngle());
     	/*if(SmartDashboard.getBoolean("M2")){
     		//motor2.set(0.5);
