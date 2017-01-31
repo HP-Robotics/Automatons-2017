30e04c00c03bdba61b350492bb32acd8e40bfc97
Team 2823 Programmers <teamautomatons2823@gmail.com>
2017-01-31 15:00:15 -0600

    Updated ToggleSwitch Class
    Updated the ToggleSwitch to become clearer and easier to use.

--- src/org/usfirst/frc/team2823/robot/ToggleSwitch.java
+++ src/org/usfirst/frc/team2823/robot/ToggleSwitch.java
@@ -1,31 +1,35 @@
 package org.usfirst.frc.team2823.robot;
 
 public class ToggleSwitch {
-		private boolean state = false;
-		private boolean previousState = false;
-		
-		/* return whether the mode switch is enabled */
-		public boolean switchEnabled() {
-			return state;
-		}
-		
-		/* update the mode switch based on this tick's controller button state.
-		 * returns whether the state changed */
-		public boolean updateState(boolean btnState){
-			boolean stateChanged = false;
-			
-			if(btnState && (btnState != previousState)) {
-				state = !state;
-				stateChanged = true;
-			}
-			
-			previousState = btnState;
-			
-			return stateChanged;
-		}
-		
-		public void reset() {
-			state = false;
-			previousState = false;
-		}
+	private boolean s = false;
+	private boolean ls = false;
+	private boolean c = false;
+	
+	public boolean on() {
+		return s;
 	}
+	
+	public boolean changed() {
+		return c;
+	}
+	
+	public void update(boolean b) {
+		
+		if(b && (b != ls)) {
+			s = !s;
+			c = true;
+			
+		} else {
+			c = false;
+		}
+		
+		ls = b;
+	}
+	
+	public void reset() {
+		s = false;
+		ls = false;
+		c = false;
+	}
+
+}
